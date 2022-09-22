03780e305819c72648fe65843c017ba02a05c3e2
Maria del Carmen <marytola.com123@gmail.com>
2022-09-22 09:58:10 -0400

    botonoes Recontraultraterminados

---
+++ index.js
@@ -0,0 +1,40 @@
+import { getContentCards }  from './assets/peticion.js'
+import { createCard } from './assets/createCard.js'
+import { mount }  from './assets/mount.js'
+import { createButton } from './assets/createButton.js'
+import { buttonDisabled } from './assets/buttonDisabled.js'
+
+const maxCards = 3
+const state={
+    quantity: maxCards,
+}
+
+//Crear botones
+createButton("Add Card", "addCard")
+createButton("Remove Card", "removeCard")
+
+//Petición del contenido de las tarjetas
+const contentCard = getContentCards()
+
+//Construcción de las tarjetas
+contentCard.forEach(item => { 
+    const card = createCard(item)
+    mount(card)
+});
+buttonDisabled(state.quantity)
+
+const addCard=document.querySelector("#addCard")
+addCard.addEventListener('click',()=>{
+    const card = createCard(contentCard[state.quantity])
+    mount(card)
+    state.quantity++
+    buttonDisabled(state.quantity)
+})
+
+const removeCard=document.querySelector("#removeCard")
+removeCard.addEventListener('click',()=>{
+    const parent = document.querySelector('.columnas')
+    parent.removeChild(parent.lastElementChild)
+    state.quantity--
+    buttonDisabled(state.quantity)
+})
